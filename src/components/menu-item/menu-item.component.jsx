32ba807882d3c96dec133db4225a63c5c5d436d9
lennoxstark47 <twisamhaldar@gmail.com>
2021-03-04 20:49:11 +0530

    added font and styling

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -3,12 +3,11 @@ import "../menu-item/menu-item.styles.scss"
 
 const MenuItem = ({title , image, size}) => {
     return (
-        <div 
-            style={
-                {backgroundImage: `url(${image})`}
-            }
-            className={`${size} menu-item`}
-        >
+        <div className={`${size} menu-item`}>
+            <div 
+                className="background-image"
+                style={{backgroundImage: `url(${image})`}}
+            />
             <div className="content">
                 <h1 className="title">{title}</h1>
                 <span className="subtitle">SHOP NOW</span>
