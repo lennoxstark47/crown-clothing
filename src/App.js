ad8bae283094c4a6b40e3b0be731361b2646c4f8
lennoxstark47 <twisamhaldar@gmail.com>
2021-03-22 14:32:22 +0530

    added Header and Link and Respective Styling

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Switch } from "react-router-dom";
 import './App.css';
 import HomePage from "../src/pages/hompage/homepage.component";
 import ShopPage from "../src/pages/shop/shop.component";
+import Header from "./components/header/header.component";
 
 
 
 function App() {
   return (
     <div >
+      <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/shop" component={ShopPage} />
