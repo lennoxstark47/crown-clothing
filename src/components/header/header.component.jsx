e4f822ce190cda558f598bba8940d2d7a638bc81
lennoxstark47 <twisamhaldar@gmail.com>
2021-04-03 20:57:36 +0530

    added cart item (incomplete)

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,8 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { auth } from '../../firebase/firebase.utils';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component'
 import './header.style.scss';
 
 const Header = ({ currentUser }) => (
@@ -26,7 +28,9 @@ const Header = ({ currentUser }) => (
                 SIGN IN
                 </Link>)
             }
+            <CartIcon />
         </div>
+        <CartDropdown />
     </div>
 );
 
